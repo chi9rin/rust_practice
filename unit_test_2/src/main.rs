ee15157ae4151205aa1d1d79d5b6f193c947b6d7
Shinya TAKEBAYASHI <omoikanenomikoto@gmail.com>
2018-03-25 07:59:26 +0900

    unit test 2

---
+++ unit_test_2/src/main.rs
@@ -0,0 +1,42 @@
+pub fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0, "m and n should be > 0");
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+
+fn main() {
+    let m = 55;
+    let n = 30;
+    println!("GCD of {} and {} is {}", m, n, gcd(m, n));
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::gcd;
+
+    #[test]
+    fn test_gcd() {
+        assert_eq!(gcd(14, 15), 1);
+        
+        assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+                       3 * 7 * 11 * 13 * 19),
+                   3 * 11);
+    }
+
+
+    #[test]
+    #[should_panic(expected = "m and n should be > 0")]
+    fn test_gcd_zero() {
+        assert_eq!(gcd(0, 1), 0)
+    }
+
+}
